460271e43da17f1c795f3d1dbfca98ccb869783c
patricktyndall <ptrck.wckhm@gmail.com>
2020-11-02 09:11:02 -0500

    catchall bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,19 @@
-export default () => 'Index Page';
+import Link from 'next/link';
+
+export default () => {
+
+	const paths = [
+		'/dynamic-1',
+		'/dynamic-2',
+	];
+
+	return (
+		<>
+			{
+				paths.map(p => (
+					<Link href={p}><a className='db'>{p}</a></Link>
+				))
+			}
+		</>
+	);
+};
